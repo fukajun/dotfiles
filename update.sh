c126b591950cff00ad12b2de4533688095ad72fa
fukajun <fukajun@gmail.com>
2012-01-17 22:38:53 +0900

    .Xmodmapなどを追加

--- update.sh
+++ update.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
+ORIGIN_DIR=`pwd`
 cd `dirname $0`
 SCRIPT_DIR=`pwd`
 TARGET_DIR=$HOME
+cd $ORIGIN_DIR
+echo $HOME
 
-[ "$1" == "get" ] && dis_dir=$SCRIPT_DIR && src_dir=$TARGET_DIR
-[ "$1" == "set" ] && dis_dir=$TARGET_DIR && src_dir=$SCRIPT_DIR
+if [ "$1" = "get" ]; then
+  dis_dir=$SCRIPT_DIR
+  src_dir=$TARGET_DIR
+elif [ "$1" = "set" ]; then
+  dis_dir=$TARGET_DIR
+  src_dir=$SCRIPT_DIR
+else
+  exit
+fi
 
 for file in `ls -a $SCRIPT_DIR | grep "^\.\w"`
 do
   echo   $src_dir/$file $dis_dir/
   cp -rp $src_dir/$file $dis_dir/
 done
-cp -rp $src_dir/gitconfig $dis_dir/
+#cp -rp $src_dir/gitconfig $dis_dir/
 
